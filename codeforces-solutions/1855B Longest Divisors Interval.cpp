c41091cc05e7a0e5be5aa0debd26fb3993e9d00d
Vaibhav Yadav <tron21.vaibhav@gmail.com>
2024-08-05 23:47:47 +0000

    Solution Upload

---
+++ codeforces-solutions/1855B Longest Divisors Interval.cpp
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Author              :- Vaibhav Yadav (https://www.linkedin.com/in/vaibhav-yadav-4397351b9/)
+// Day_of_submission   :- Monday
+// Date_of_submission  :- 2024-08-05
+// Time_of_submission  :- 22:47:46
+// Time_taken          :- 77 ms
+// Memory_consumed     :- 0 KB
+// Testcase_passed     :- 12
+// Status              :- Submitted
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define loop(i, a, n) for (int i = a; i < n; i++)
+#define ll long long
+#define MX 10000007
+#define all(v) v.begin(),v.end()
+
+
+
+void solve(){
+    ll n;
+    cin>>n;
+    
+    ll ans;
+    for(int i=1; ;i++){
+        if(n%i!=0){
+            ans = i-1;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+
+
+int main(){
+    IOS;
+    ll t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+
+// ------ STAR THIS REPO IF YOU FOUND HELPFUL ---------
+// धन्यवाद !
